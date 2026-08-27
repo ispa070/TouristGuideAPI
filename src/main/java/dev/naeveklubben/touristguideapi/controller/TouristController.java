19703af44740c08dd989dd87e85ce50b0b48e303
Villa1123 <vich1001@stud.ek.dk>
2026-08-27 15:03:21 +0200

    ORALE controller<3

--- src/main/java/dev/naeveklubben/touristguideapi/controller/TouristController.java
+++ src/main/java/dev/naeveklubben/touristguideapi/controller/TouristController.java
@@ -1,4 +1,40 @@
 package dev.naeveklubben.touristguideapi.controller;
 
+import dev.naeveklubben.touristguideapi.model.TouristAttraction;
+import dev.naeveklubben.touristguideapi.service.TouristService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/attractions")
+
 public class TouristController {
+    private final TouristService service;
+
+    public TouristController(TouristService service){
+        this.service = service;
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<TouristAttraction>> getAttraction(){
+        List<TouristAttraction> attractions = service.getAttractions();
+        return new ResponseEntity<>(attractions, HttpStatus.OK);
+    }
+
+    @GetMapping("{name}")
+        public ResponseEntity<TouristAttraction> getAttractionByName(@PathVariable String name) {
+        TouristAttraction touristAttraction = service.findTouristAttractionByName(name);
+        if (touristAttraction == null) {
+            return new ResponseEntity<>(touristAttraction, HttpStatus.NOT_FOUND);
+        } else {
+            return new ResponseEntity<>(touristAttraction, HttpStatus.OK);
+        }
+    }
+
 }
