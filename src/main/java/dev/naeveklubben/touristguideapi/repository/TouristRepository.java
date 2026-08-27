b682bcc956e70a4c2631e7eb824e22180921a991
nickichandk <nicklas.marc@gmail.com>
2026-08-27 14:19:40 +0200

    Model package done

--- src/main/java/dev/naeveklubben/touristguideapi/repository/TouristRepository.java
+++ src/main/java/dev/naeveklubben/touristguideapi/repository/TouristRepository.java
@@ -1,8 +1,31 @@
 package dev.naeveklubben.touristguideapi.repository;
 
+import dev.naeveklubben.touristguideapi.model.TouristAttraction;
 import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Repository
 public class TouristRepository {
+    private final List<TouristAttraction> attractions = new ArrayList<>();
+    private String attractionName;
+
+
+    public List<TouristAttraction> getAllAttractions(){
+        return attractions;
+    }
+
+    public TouristAttraction findAttractionByName(String name){
+        for(TouristAttraction attraction : attractions){
+            if(attraction.getName().equals(name)){
+                return attraction;
+            }
+        }
+        return null;
+    }
+
+
+
 
 }
