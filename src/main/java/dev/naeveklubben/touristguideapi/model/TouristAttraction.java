2f7b3255ce31ef3185ceedc04ab187c83ba6f8a4
nickichandk <nicklas.marc@gmail.com>
2026-08-27 13:59:35 +0200

    Model package done

--- src/main/java/dev/naeveklubben/touristguideapi/model/TouristAttraction.java
+++ src/main/java/dev/naeveklubben/touristguideapi/model/TouristAttraction.java
@@ -4,4 +4,18 @@ public class TouristAttraction {
     private String name;
     private String description;
 
+    public TouristAttraction(String name, String description){
+        this.name = name;
+        this.description = description;
+    }
+
+
+    public String getName(){
+        return name;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
 }
